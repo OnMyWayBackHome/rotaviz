bfca385b92ab8f327552403cbae45be172ac5fb5
Laszlo C Bardos <laszlo@bardos.us>
2017-09-03 17:40:17 -0400

    Arduino test of HT16K33 chip

---
+++ test_ht16k33/test_ht16k33.ino
@@ -0,0 +1,12 @@
+/* Test writing to the HT16K33 chip to light LEDs.
+ * 
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
